d5fbde0dbaf05085142e1d2d009ab2fa945b4963
saumya <2saumyaray@gmail.com>
2021-01-05 22:12:41 +0530

    making a proper folder for the web host to serve the files

--- 3.go
+++ 3.go
@@ -18,8 +18,8 @@ func ping(w http.ResponseWriter, r *http.Request){
 //
 // Application Entry
 func main(){
-	fmt.Println("Go 103 : File Server")
-	http.Handle("/", http.FileServer(http.Dir("./")))
+	fmt.Println("Go 103: File Server running at port: 8000")
+	http.Handle("/", http.FileServer(http.Dir("./webhome/")))
 	http.HandleFunc("/ping", ping)
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		panic(err)
