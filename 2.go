71aa62b6f17f5edcc2ed005d8416974f94717a7b
saumya <2saumyaray@gmail.com>
2021-01-05 19:38:38 +0530

    study: moving on with GO

---
+++ 2.go
@@ -0,0 +1,10 @@
+//
+// run this : go run 2.go
+//
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Go 102")
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module 1_letmeGo
 
 go 1.12
 
-require rsc.io/quote v1.5.2 // indirect
+require rsc.io/quote v1.5.2
